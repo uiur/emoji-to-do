276b8c593969a1f84e408b93c41a25e85f7141a9
Kazato Sugimoto <uiureo@gmail.com>
2022-06-15 22:42:08 +0900

    Refactor using struct methods

--- src/handlers/webhook.rs
+++ src/handlers/webhook.rs
@@ -13,10 +13,48 @@ struct TeamConfig {
   reaction_patterns: Vec<ReactionPattern>,
 }
 
+#[derive(Clone)]
 struct ReactionPattern {
   name: String,
+  repo: String,
   assignees: Vec<String>,
 }
+
+struct TeamConfigMap {
+  data: HashMap<String, TeamConfig>,
+}
+
+impl TeamConfigMap {
+  fn new() -> TeamConfigMap {
+    let mut config_map = HashMap::new();
+    config_map.insert(
+      "T1NRWJ5QT".to_string(),
+      TeamConfig {
+        team_id: "T1NRWJ5QT".to_string(),
+        repo: "uiur/private-sandbox".to_string(),
+        reaction_patterns: vec![
+          ReactionPattern { name: String::from("memo"), repo: "uiur/private-sandbox".to_string(), assignees: vec![] }
+        ],
+      }
+    );
+
+    TeamConfigMap { data: config_map }
+  }
+
+  fn find(self, team_id: &str, channel: &str, reaction: &str) -> Option<ReactionPattern> {
+    let team_config = self.data.get(team_id);
+    match team_config {
+        Some(c) => {
+          c.reaction_patterns.iter().find(|reaction_pattern| reaction == reaction_pattern.name)
+            .and_then(|p| Some(p.clone()))
+        },
+        None => {
+          return None;
+        },
+    }
+  }
+}
+
 
 #[post("/webhook/slack/events")]
 pub async fn create_slack_events(data: web::Json<SlackRequest>, req: HttpRequest) -> actix_web::Result<impl Responder> {
@@ -28,17 +66,7 @@ pub async fn create_slack_events(data: web::Json<SlackRequest>, req: HttpRequest
     }
 
     info!("{:#?}", data);
-    let mut config_map = HashMap::new();
-    config_map.insert(
-      "T1NRWJ5QT".to_string(),
-      TeamConfig {
-        team_id: "T1NRWJ5QT".to_string(),
-        repo: "uiur/private-sandbox".to_string(),
-        reaction_patterns: vec![
-          ReactionPattern { name: String::from("memo"), assignees: vec![] }
-        ],
-      }
-    );
+    let config_map = TeamConfigMap::new();
 
     match data.0 {
         SlackRequest::UrlVerification { challenge } => {
@@ -51,11 +79,7 @@ pub async fn create_slack_events(data: web::Json<SlackRequest>, req: HttpRequest
               let reactioner = slack::get_user_info(&user).await?;
 
               let team_id = reactioner.team_id;
-              let config = config_map.get(&team_id).unwrap();
-
-              if !config.reaction_patterns.iter().any(|p| p.name == reaction) {
-                return Ok(HttpResponse::Ok().into());
-              }
+              let pattern = config_map.find(&team_id, "", &reaction).unwrap();
 
               if let SlackItem::Message { channel, ts } = item {
                 let messages = slack::get_messages(&channel, &ts, 3).await
@@ -79,7 +103,7 @@ pub async fn create_slack_events(data: web::Json<SlackRequest>, req: HttpRequest
                 let title: String = messages.first().and_then(|m| Some(String::from(&m.text))).unwrap_or_default();
 
                 let body = format!("```\n{}\n```\n{}", text, permalink);
-                let issue = github::create_issue(&config.repo, &title, &body).await?;
+                let issue = github::create_issue(&pattern.repo, &title, &body).await?;
 
                 slack::post_message(&channel, &format!("<@{}> {}", reactioner.name, issue.html_url)).await
                   .map_err(|_| actix_web::error::ErrorInternalServerError(""))?;
