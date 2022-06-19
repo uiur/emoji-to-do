eec018e279d2ef223533fc3af586efe2fcbeb3ad
Kazato Sugimoto <uiureo@gmail.com>
2022-06-19 21:40:22 +0900

    Create front/ dir

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>emoji-to-do</p>
+    </div>
+  );
+}
+
+export default App;
