a0a38b04ef4771979686b5855d8469624360c59c
Kazato Sugimoto <uiureo@gmail.com>
2022-07-18 16:28:04 +0900

    Add form for reaction assignees

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -28,6 +28,12 @@ interface Reaction {
   id: number
   name: string
   repo: string
+  reaction_assignees: ReactionAssignee[]
+}
+
+interface ReactionAssignee {
+  id: number
+  name: string
 }
 
 const fetchWithToken = async (url: string, token: string | null) => {
@@ -104,6 +110,107 @@ function ReactionForm({
   )
 }
 
+function ReactionAssigneeForm({
+  reaction,
+  onSave,
+}: {
+  reaction: Reaction
+  onSave: () => void
+}) {
+  const [name, setName] = useState('')
+  const token = useContext(TokenContext)
+  const [errorMessage, setErrorMessage] = useState('')
+  const onSubmit = useCallback(async () => {
+    const res = await client
+      .post(
+        `/api/reactions/${reaction.id}/reaction_assignees`,
+        {
+          name: name,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((err) => {
+        setErrorMessage(err.message)
+      })
+
+    setName('')
+    setErrorMessage('')
+    onSave()
+  }, [reaction.id, name])
+
+  return (
+    <form
+      className="flex flex-col"
+      onSubmit={(e) => {
+        e.preventDefault()
+        onSubmit()
+      }}
+    >
+      <div className="flex">
+        <input
+          className="bg-gray-200 p-2 flex-1"
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.currentTarget.value)}
+        ></input>
+
+        <input
+          className="bg-blue-600 text-white cursor-pointer p-2 ml-2"
+          type="submit"
+          value="Add"
+          disabled={!name}
+        ></input>
+      </div>
+
+      {errorMessage.length > 0 && (
+        <div className="text-red-600">{errorMessage}</div>
+      )}
+    </form>
+  )
+}
+
+function ReactionAssigneeComponent({
+  reactionAssignee,
+  onDelete
+}: {
+  reactionAssignee: ReactionAssignee,
+  onDelete: () => void
+}) {
+  const token = useContext(TokenContext)
+  const deleteOnClick = useCallback(async () => {
+    await client
+    .delete(
+      `/api/reaction_assignees/${reactionAssignee.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+
+    onDelete()
+
+  }, [reactionAssignee.id])
+
+  return (
+    <span key={reactionAssignee.id}>
+      {reactionAssignee.name}
+
+      <span
+        className="mx-2 cursor-pointer underline"
+        onClick={deleteOnClick}
+      >
+        delete
+      </span>
+    </span>
+  )
+}
+
 function Content() {
   const token = useContext(TokenContext)
   const { data: user } = useSWR<User>('/api/user', (url) =>
@@ -133,14 +240,18 @@ function Content() {
       {user && (
         <div>
           <h2>user</h2>
-          <p>{user.id} {user.slack_user_id}</p>
+          <p>
+            {user.id} {user.slack_user_id}
+          </p>
         </div>
       )}
 
       {team && (
         <div>
           <h2>team</h2>
-          <p>{team.id} {team.name}</p>
+          <p>
+            {team.id} {team.name}
+          </p>
         </div>
       )}
 
@@ -151,33 +262,49 @@ function Content() {
           {(reactions || []).map((reaction) => {
             return (
               <li key={reaction.id}>
-                <span>
-                  id: {reaction.id}, name: {reaction.name}, repo:{' '}
-                  <a
-                    href={`https://github.com/${reaction.repo}`}
-                    target="_blank"
+                <div>
+                  <span>
+                    id: {reaction.id}, name: {reaction.name}, repo:{' '}
+                    <a
+                      href={`https://github.com/${reaction.repo}`}
+                      target="_blank"
+                    >
+                      {reaction.repo}
+                    </a>
+                  </span>
+                  <span
+                    className="mx-2 cursor-pointer underline"
+                    onClick={() => deleteOnClick(reaction)}
                   >
-                    {reaction.repo}
-                  </a>
-                </span>
-                <span
-                  className="mx-2 cursor-pointer underline"
-                  onClick={() => deleteOnClick(reaction)}
-                >
-                  delete
-                </span>
+                    delete
+                  </span>
+                </div>
+                <div>
+                  {reaction.reaction_assignees.map((reactionAssignee) => {
+                    return (
+                      <ReactionAssigneeComponent key={reactionAssignee.id} reactionAssignee={reactionAssignee} onDelete={mutateReactions} />
+                    )
+                  })}
+                  <ReactionAssigneeForm
+                    reaction={reaction}
+                    onSave={mutateReactions}
+                  />
+                </div>
               </li>
             )
           })}
         </ul>
 
         {team && (
-          <ReactionForm
-            team={team}
-            onSave={() => {
-              mutateReactions()
-            }}
-          />
+          <div className="mt-1">
+            <h2 className="font-semibold">create reaction</h2>
+            <ReactionForm
+              team={team}
+              onSave={() => {
+                mutateReactions()
+              }}
+            />
+          </div>
         )}
       </section>
     </div>
